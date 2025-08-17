1baabe8177718d59d2d4c1255a6a8c58debff67a
kang chang min <kcmschool73@gmail.com>
2025-08-17 21:43:08 +0900

    패키지 의존성 키파일로 변경

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+const { initOpenNextCloudflareForDev } = require("@opennextjs/cloudflare");
+initOpenNextCloudflareForDev();
+
+module.exports = nextConfig;
