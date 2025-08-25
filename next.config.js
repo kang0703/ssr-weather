2c4eb342c3afb94ead9f37076516b841d6d00776
kang chang min <kcmschool73@gmail.com>
2025-08-25 16:07:02 +0900

    이미지 최적화

--- next.config.js
+++ next.config.js
@@ -4,10 +4,21 @@ const nextConfig = {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30일 캐시
   },
   // 성능 최적화
   experimental: {
     optimizeCss: true,
+    // 추가 최적화
+    optimizePackageImports: ['@heroicons/react'],
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
   },
   // 압축 설정
   compress: true,
